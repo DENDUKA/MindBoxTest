f550f456aefe0712d5fcd69df30e618b2ce0df04
DENDUKA <49064635+DENDUKA@users.noreply.github.com>
2024-02-21 14:01:46 +0400

    Final

--- Task1/MindBoxTest/Program.cs
+++ Task1/MindBoxTest/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MindBox.Library;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,16 @@ namespace MindBoxTest
     {
         static void Main(string[] args)
         {
+            IFigure figure1 = new Circle(20);
+            IFigure figure2 = new Triangle(5, 6, 7);
+
+            var area1 = figure1.GetArea();
+            var area2 = figure2.GetArea();
+
+            Triangle triangle = figure2 as Triangle;
+
+            triangle.IsRectangular();
+
         }
     }
 }
